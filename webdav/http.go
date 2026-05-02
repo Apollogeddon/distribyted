16c92ea43633ef8722e08479fa1168f76c7cf969
Apollogeddon <dunc1221@proton.me>
2026-05-02 16:57:33 +0930

    feat: modularize WebDAV server and handler

--- webdav/http.go
+++ webdav/http.go
@@ -2,6 +2,7 @@ package webdav
 
 import (
 	"fmt"
+	"net"
 	"net/http"
 
 	"github.com/Apollogeddon/distribyted/fs"
@@ -11,10 +12,16 @@ import (
 
 func NewWebDAVServer(fs fs.Filesystem, port int, user, pass string) error {
 	log.Info().Str(dlog.KeyHost, fmt.Sprintf("0.0.0.0:%d", port)).Msg("starting webDAV server")
+	return http.ListenAndServe(fmt.Sprintf("0.0.0.0:%d", port), NewWebDAVHandler(fs, user, pass))
+}
 
+func NewWebDAVServerWithListener(l net.Listener, fs fs.Filesystem, user, pass string) error {
+	return http.Serve(l, NewWebDAVHandler(fs, user, pass))
+}
+
+func NewWebDAVHandler(fs fs.Filesystem, user, pass string) http.Handler {
 	srv := newHandler(fs)
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		username, password, _ := r.BasicAuth()
 		if username == user && password == pass {
 			srv.ServeHTTP(w, r)
@@ -25,6 +32,4 @@ func NewWebDAVServer(fs fs.Filesystem, port int, user, pass string) error {
 		w.WriteHeader(401)
 		_, _ = w.Write([]byte("401 Unauthorized\n"))
 	})
-
-	return http.ListenAndServe(fmt.Sprintf("0.0.0.0:%d", port), nil)
 }
